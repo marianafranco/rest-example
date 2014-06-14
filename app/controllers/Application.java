f8dc6de03377489063cf7f7d47c6c7ae2d5779b7
Mariana Franco <ramos.franco@gmail.com>
2014-06-14 20:00:52 -0300

    POST and PUT methods

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import model.Car;
 import play.cache.Cache;
+import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -39,15 +41,70 @@ public class Application extends Controller {
 
     public static Result getCar(String manufacturer, String model) {
     	List<Car> cars = (List<Car>) Cache.get("cars");
-    	List<Car> result = new ArrayList<Car>();
+    	Car result = null;
     	for (Car car : cars) {
     		if (car.getManufacturer().equals(manufacturer)
     				&& car.getModel().equals(model)) {
-    			result.add(car);
+    			result = car;
+    			break;
     		}
     	}
+    	if (result == null) {
+    		return notFound();
+    	} else {
+    		ObjectMapper mapper = new ObjectMapper();
+        	JsonNode node = mapper.convertValue(result, JsonNode.class);
+        	return ok(node);
+    	}
+    }
+    
+    @BodyParser.Of(BodyParser.Json.class)
+    public static Result addCar() throws JsonProcessingException {
+    	JsonNode node = request().body().asJson();
     	ObjectMapper mapper = new ObjectMapper();
-    	JsonNode node = mapper.convertValue(result, JsonNode.class);
-    	return ok(node);
+        Car newCar = mapper.treeToValue(node, Car.class);
+        if (newCar.getManufacturer() == null || newCar.getModel() == null) {
+        	return badRequest("Invalid car data.");
+        }
+        List<Car> cars = (List<Car>) Cache.get("cars");
+        for(Car car : cars) {
+        	if(car.getManufacturer().equals(newCar.getManufacturer())
+        			&& car.getModel().equals(newCar.getModel())) {
+        		return status(CONFLICT);
+        	}
+        }
+        cars.add(newCar);
+        Cache.set("cars", cars);
+    	return created();
+    }
+
+    @BodyParser.Of(BodyParser.Json.class)
+    public static Result updateCar(String manufacturer, String model)
+    		throws JsonProcessingException {
+    	JsonNode node = request().body().asJson();
+    	ObjectMapper mapper = new ObjectMapper();
+        Car newCar = mapper.treeToValue(node, Car.class);
+        if (newCar.getManufacturer() == null || newCar.getModel() == null) {
+        	return badRequest("Invalid car data.");
+        }
+        
+        boolean found = false;
+        List<Car> cars = (List<Car>) Cache.get("cars");
+        for(Car car : cars) {
+        	if(car.getManufacturer().equals(newCar.getManufacturer())
+        			&& car.getModel().equals(newCar.getModel())) {
+        		cars.remove(car);
+        		found = true;
+        		break;
+        	}
+        }
+        
+        if(!found) {
+        	return notFound();
+        } else {
+        	cars.add(newCar);
+        	Cache.set("cars", cars);
+        	return ok();
+        }
     }
 }
